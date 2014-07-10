20ec371079377dbbd2d017ae1f5f71252cf95f36
gurieveugen <gurievinvest@gmail.com>
2014-07-10 22:34:13 +0300

    0.002

---
+++ list.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Beregovo.in</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<header class="main">
+		<div class="container">
+			<div class="banner">
+				<a href="#">
+					<img src="http://placehold.it/730x90/b1b1b1/ffffff" alt="banner"></a>
+			</div>
+			<div class="logo">
+				<a href="/">
+					<img src="images/logo.png" alt="Logo"></a>
+			</div>
+		</div>
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Новини</a>
+				</li>
+				<li>
+					<a href="#">Аналітика</a>
+				</li>
+				<li>
+					<a href="#">Реклама</a>
+				</li>
+				<li>
+					<a href="#">Контакти</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<section class="main">
+		<div class="container">
+			<div class="column2x">
+				<h1>Новини</h1>
+				<article class="short-news">
+					<div class="img-block">
+						<img src="http://placehold.it/100x72/b1b1b1/ffffff" alt="thumb">
+					</div>
+					<div class="text-block">
+						<h2><a href="#">Some title</a></h2>
+						<div class="text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, placeat, optio adipisci ipsa mollitia ipsam aliquid totam ut minus deleniti esse temporibus eos consequuntur neque quam nam similique veritatis cum!
+						</div>
+					</div>
+				</article>
+
+				<article class="short-news">
+					<div class="img-block">
+						<img src="http://placehold.it/100x72/b1b1b1/ffffff" alt="thumb">
+					</div>
+					<div class="text-block">
+						<h2><a href="#">Some title</a></h2>
+						<div class="text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, placeat, optio adipisci ipsa mollitia ipsam aliquid totam ut minus deleniti esse temporibus eos consequuntur neque quam nam similique veritatis cum!
+						</div>
+					</div>
+				</article>
+
+				<article class="short-news">
+					<div class="img-block">
+						<img src="http://placehold.it/100x72/b1b1b1/ffffff" alt="thumb">
+					</div>
+					<div class="text-block">
+						<h2><a href="#">Some title</a></h2>
+						<div class="text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, placeat, optio adipisci ipsa mollitia ipsam aliquid totam ut minus deleniti esse temporibus eos consequuntur neque quam nam similique veritatis cum!
+						</div>
+					</div>
+				</article>
+
+				<article class="short-news">
+					<div class="img-block">
+						<img src="http://placehold.it/100x72/b1b1b1/ffffff" alt="thumb">
+					</div>
+					<div class="text-block">
+						<h2><a href="#">Some title</a></h2>
+						<div class="text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, placeat, optio adipisci ipsa mollitia ipsam aliquid totam ut minus deleniti esse temporibus eos consequuntur neque quam nam similique veritatis cum!
+						</div>
+					</div>
+				</article>
+
+				<article class="short-news">
+					<div class="img-block">
+						<img src="http://placehold.it/100x72/b1b1b1/ffffff" alt="thumb">
+					</div>
+					<div class="text-block">
+						<h2><a href="#">Some title</a></h2>
+						<div class="text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, placeat, optio adipisci ipsa mollitia ipsam aliquid totam ut minus deleniti esse temporibus eos consequuntur neque quam nam similique veritatis cum!
+						</div>
+					</div>
+				</article>
+				<ul class="pagination">
+					<li><a href="#">&#8592;</a></li>
+					<li><a href="#" class="active">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li><a href="#">4</a></li>
+					<li><a href="#">5</a></li>
+					<li><a href="#">6</a></li>
+					<li><a href="#">7</a></li>
+					<li><a href="#">8</a></li>
+					<li><a href="#">9</a></li>
+					<li><a href="#">10</a></li>
+					<li><a href="#">&#8594;</a></li>
+				</ul>
+				<div class="clear"></div>	
+				<div class="disqus">
+					<div id="disqus_thread"></div>
+					<script type="text/javascript">
+					    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+					    var disqus_shortname = 'irshavanews'; // required: replace example with your forum shortname
+
+					    /* * * DON'T EDIT BELOW THIS LINE * * */
+					    (function() {
+					        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+					        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+					    })();
+					</script>
+					<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+					<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+					
+				</div>
+			</div>
+			<div class="column">
+				<a href="#" class="banner">
+					<img src="http://placehold.it/300x250/b1b1b1/ffffff" alt="banner"></a>
+				<?php include('sinoptik.php'); ?>
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<div class="block-article">
+								<figure>
+									<img src="images/placeholders/1.jpg" alt="Obama">
+									<figcaption class="bg-green">Вибір редакції</figcaption>
+								</figure>
+								<a href="#" class="title">Цитуючи Вакарчука, Обама запевнив, що все буде добре!</a>
+								<span class="description">
+									Президент Сполучених Штатів Америки запевнив, що любить Океан Ельзи і старі альбоми Motorhead
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="block-article">
+								<figure>
+									<img src="images/placeholders/2.jpg" alt="Obama">
+									<figcaption class="bg-green">Вибір редакції</figcaption>
+								</figure>
+								<a href="#" class="title">
+									Українці заявили, що замість грошей, вишлють Путіну Владислава Лакатоша
+								</a>
+								<span class="description">
+									Президент Сполучених Штатів Америки запевнив, що любить Океан Ельзи і старі альбоми Motorhead
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="block-article">
+								<figure>
+									<img src="images/placeholders/3.jpg" alt="Obama">
+									<figcaption class="bg-green">Вибір редакції</figcaption>
+								</figure>
+								<a href="#" class="title">
+									Якщо в крані нема води, значить десь її дві. Або як боротися зі спрагою за рахунок сусіда.
+								</a>
+								<span class="description">
+									Президент Сполучених Штатів Америки запевнив, що любить Океан Ельзи і старі альбоми Motorhead
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="block-article">
+								<figure>
+									<img src="images/placeholders/4.jpg" alt="Obama">
+									<figcaption class="bg-green">Вибір редакції</figcaption>
+								</figure>
+								<a href="#" class="title">
+									Факт - є факт. Ліверпуль - найкращий футбольний клуб всіх часів. Заперечення не приймаються.
+								</a>
+								<span class="description">
+									Президент Сполучених Штатів Америки запевнив, що любить Океан Ельзи і старі альбоми Motorhead
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="block-article">
+								<figure>
+									<img src="images/placeholders/1.jpg" alt="Obama">
+									<figcaption class="bg-green">Вибір редакції</figcaption>
+								</figure>
+								<a href="#" class="title">На цьому фото я  розвернутий в іншу сторону» - Б. Обама</a>
+								<span class="description">
+									Президент Сполучених Штатів Америки запевнив, що любить Океан Ельзи і старі альбоми Motorhead
+								</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<a href="#" class="banner">
+					<img src="http://placehold.it/300x250/b1b1b1/ffffff" alt="banner"></a>
+				<div class="block-tabs">
+					<ul class="links">
+						<li>
+							<a class="active" data-id="#fb-tab" href="#">Facebook</a>
+						</li>
+						<li>
+							<a data-id="#vk-tab" href="#" class="">VK</a>
+						</li>
+					</ul>
+					<ul class="tabs">
+						<li class="show" id="fb-tab">
+							<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FMukachevo.net&amp;width&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false&amp;appId=732484396795127" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:258px;" allowTransparency="true"></iframe>
+						</li>
+						<li id="vk-tab" class="">
+							<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FMukachevo.net&amp;width&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false&amp;appId=732484396795127" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:258px;" allowTransparency="true"></iframe>
+						</li>
+					</ul>
+				</div>
+				<div class="block-news">
+					<h1>Новини</h1>
+					<article>
+						<a href="#" class="title">
+							В Межигі’ї довели, що рукописи, все ж таки, не горять. Тепер Воланд є персоною нон-грата, ще на 20000 років.
+						</a>
+					</article>
+					<article>
+						<a href="#" class="title">
+							Все, що тебе стосується. Все, що мене стосується. Все тільки починається, все тільки починається!
+						</a>
+					</article>
+					<article>
+						<a href="#" class="title">
+							В Межигі’ї довели, що рукописи, все ж таки, не горять. Тепер Воланд є персоною нон-грата, ще на 20000 років.
+						</a>
+					</article>
+					<article>
+						<a href="#" class="title">
+							Все, що тебе стосується. Все, що мене стосується. Все тільки починається, все тільки починається!
+						</a>
+					</article>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="main" style="margin-top: 150px;">
+		<div class="container">
+			<a href="/" class="logo">
+				<img src="images/logo_footer.png" alt="Logo"></a>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">Новини</a>
+					</li>
+					<li>
+						<a href="#">Аналітика</a>
+					</li>
+					<li>
+						<a href="#">Реклама</a>
+					</li>
+					<li>
+						<a href="#">Контакти</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</footer>
+</body>
+</html>
